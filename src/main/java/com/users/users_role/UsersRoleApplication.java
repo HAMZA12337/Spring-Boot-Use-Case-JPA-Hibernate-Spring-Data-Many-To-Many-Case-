6db56bdb0db36735fe0e73901869c893b35460fe
Your Name <you@example.com>
2023-03-27 02:20:17 +0000

    first

---
+++ src/main/java/com/users/users_role/UsersRoleApplication.java
@@ -0,0 +1,13 @@
+package com.users.users_role;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsersRoleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UsersRoleApplication.class, args);
+    }
+
+}
